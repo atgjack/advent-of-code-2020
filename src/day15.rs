dcf1d307ae6685303bf14534f5003e9983b9522b
atgJack <some@one.anon>
2020-12-17 17:31:00 -0500

    fifteen

---
+++ src/day15.rs
@@ -0,0 +1,67 @@
+use std::{collections::HashMap, str::FromStr};
+
+#[aoc_generator(day15)]
+pub fn input_generator(input: &str) -> Vec<usize> {
+    input.split(',')
+        .map(FromStr::from_str)
+        .filter_map(Result::ok)
+        .collect()
+}   
+
+fn find_nth_number(input: &[usize], n: usize) -> usize {
+    let mut hashmap = HashMap::new();
+    for (index, &value) in input.iter().enumerate() {
+        hashmap.insert(value, index + 1);
+    }
+    ((input.len() + 2)..(n + 1))
+        .scan((0, hashmap), |(current, hashmap), turn| {
+            let next = match hashmap.get(current) {
+                Some(&val) => (turn - 1) - val,
+                None => 0 
+            };
+            hashmap.insert(*current, turn - 1);
+            *current = next;
+            Some(next)
+        })
+        .last()
+        .unwrap()
+}
+
+#[aoc(day15, part1)]
+pub fn solve_part_one(input: &[usize]) -> usize {
+    find_nth_number(input, 2020)
+}
+
+#[aoc(day15, part2)]
+pub fn solve_part_two(input: &[usize]) -> usize {
+    find_nth_number(input, 30000000)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_solves_part_one() {
+        assert_eq!(solve_part_one(&input_generator("0,3,6")), 436);
+        assert_eq!(solve_part_one(&input_generator("1,3,2")), 1);
+        assert_eq!(solve_part_one(&input_generator("2,1,3")), 10);
+        assert_eq!(solve_part_one(&input_generator("1,2,3")), 27);
+        assert_eq!(solve_part_one(&input_generator("2,3,1")), 78);
+        assert_eq!(solve_part_one(&input_generator("3,2,1")), 438);
+        assert_eq!(solve_part_one(&input_generator("3,1,2")), 1836);
+    }
+    
+    #[test]
+    fn it_solves_part_two() {
+        // Disabled because -- slow
+        // assert_eq!(solve_part_two(&input_generator("0,3,6")), 175594);
+        // assert_eq!(solve_part_two(&input_generator("1,3,2")), 2578);
+        // assert_eq!(solve_part_two(&input_generator("2,1,3")), 3544142);
+        // assert_eq!(solve_part_two(&input_generator("1,2,3")), 261214);
+        // assert_eq!(solve_part_two(&input_generator("2,3,1")), 6895259);
+        // assert_eq!(solve_part_two(&input_generator("3,2,1")), 18);
+        // assert_eq!(solve_part_two(&input_generator("3,1,2")), 362);
+    }
+
+}
